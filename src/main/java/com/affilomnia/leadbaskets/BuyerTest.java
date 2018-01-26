4fccc6d8740c638578d64aafbe68a4302b404374
kirill <kirilk@affilomania.com>
2018-01-27 00:19:43 +0200

    video

--- src/main/java/com/affilomnia/leadbaskets/BuyerTest.java
+++ src/main/java/com/affilomnia/leadbaskets/BuyerTest.java
@@ -2,14 +2,12 @@ package com.affilomnia.leadbaskets;
 
 import static com.codeborne.selenide.Selenide.open;
 
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.automation.remarks.testng.VideoListener;
 import com.automation.remarks.video.annotations.Video;
-import com.codeborne.selenide.Configuration;
 import com.google.inject.Inject;
 import Pages.BuyerPage;
 import Pages.LoginPage;
@@ -20,22 +18,12 @@ import libry.BaseListener;
 
 @Guice
 @Listeners({ BaseListener.class, VideoListener.class })
-public class BuyerTest {
+public class BuyerTest extends BaseTest {
 
 	@Inject
 	LoginPage loginPage;
 	@Inject
 	BuyerPage buyerPage;
-
-	@BeforeClass
-	public void before() {
-		Configuration.startMaximized = true;
-		Configuration.screenshots = true;
-		Configuration.headless = false;
-		Configuration.holdBrowserOpen = false;
-		System.out.println("before");
-
-	}
 
 	@Video
 	@Feature("Buyer Statistic")
@@ -51,7 +39,7 @@ public class BuyerTest {
 	@Video
 	@Feature("Buyer Statistic")
 	@Severity(SeverityLevel.CRITICAL)
-	@Test(enabled = true, description = "Test total leads/spent/avgcpl par offer ", groups = { "buyer statistic" }, priority = 1)
+	@Test(enabled = false, description = "Test total leads/spent/avgcpl par offer ", groups = { "buyer statistic" }, priority = 1)
 	public void testTotalLeadTotalSpentAvgcplParCamp() throws Exception {
 		open("https://test_app.leadsbasket.com");
 		loginPage.login("kirilk+bidder@affilomania.com", "0546474985Ko");
@@ -63,7 +51,7 @@ public class BuyerTest {
 	@Video
 	@Feature("Registration")
 	@Severity(SeverityLevel.BLOCKER)
-	@Test(enabled = true, description = "Buyer Registration", groups = { "BuyerReg" }, priority = 1)
+	@Test(enabled = false, description = "Buyer Registration", groups = { "BuyerReg" }, priority = 1)
 	public void testBuyerRegistrtion() {
 		open("https://test_app.leadsbasket.com/register-industry");
 		buyerPage.industryPage();
